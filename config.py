1db079876812a99db654f7cd8487469e5c2f86a0
Sharat Hegde <sharathegde56@gmail.com>
2021-11-14 00:59:52 +0530

    implementing datanodes

--- config.py
+++ config.py
@@ -1,27 +1,45 @@
+#!/usr/bin/python3
+
 import os
+import sys
+import json
 # os.mkdir('path_to_datanodes') 
 num_of_dnodes=4
-data_node_track={}
+rep_factor = 2
+
+mapper = {}
+
+def hashing(file_block_no, num_of_datanodes):
+    return (file_block_no % num_of_datanodes) + 1
+
 # creates direcories which act as datanodes 
 def datanode_creation(num_of_datanode):
 	for data_node_number in range(1,num_of_dnodes+1):
-		os.mkdir('path_to_datanodes/'+str(data_node_number)+"_data_node")
-		data_node_track[data_node_number]=0
-		# f = open(os.path.expanduser(os.path.join(str(data_node_number) + "_data_node.txt")), "a")
-# datanode_creation(num_of_dnodes)  
+		os.mkdir('Datanodes/'+str(data_node_number)+"_data_node")
+		mapper[str(data_node_number)+"_data_node"] = {}
+#datanode_creation(num_of_dnodes)  
 
 # creates a chunk of file
-def split_files(filename, chunksize,datanode_number):
+def split_files(filename, chunksize):
+	# try:
+	# 	logs = open('Namenodes/sample.json')
+	# except Exception as e:
+	# 	logs = open('Namenodes/sample.json')
+	# mapper = json.load(logs)
+ 
 	with open(filename + '.txt', 'rb') as bytefile:
 		content = bytearray(os.path.getsize(filename + '.txt'))
 		bytefile.readinto(content)
-		
 		for count, i in enumerate(range(0, len(content), chunksize)):
-			with open('path_to_datanodes/'+str(datanode_number)+'_data_node/'+filename + '_' + str(count+1) + '.txt.', 'wb') as fh:
+			hashed = hashing(count,num_of_dnodes)
+			block_no_in_dn = len(mapper[f'{hashed}_data_node'])
+			with open(f'Datanodes/{hashed}' + '_data_node/' + str(block_no_in_dn+1) + '.txt.', 'wb') as fh:
+				mapper[f'{hashed}_data_node'][block_no_in_dn+1] = f'{filename}_{count+1}.txt'
 				fh.write(content[i: i + chunksize])
-split_files('a',5*1024,1)
-
-print(data_node_track)
-def hash_function(filename,chunksize):
-	size=os.path.getsize(filename + '.txt')
-
+		fh.close()
+	bytefile.close()
+	json.dump(mapper,logs)
+	logs.close()
+			
+split_files('xD',1000 * 1000)
+print(mapper)
