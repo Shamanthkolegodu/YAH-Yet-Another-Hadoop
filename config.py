b846b1b33af764f0d73b3984e0a2abb33437d30b
ShreyasBaasri07 <57621491+ShreyasBaasri07@users.noreply.github.com>
2021-11-15 20:56:35 +0530

    replication factor done

--- config.py
+++ config.py
@@ -7,82 +7,79 @@ import json
 num_of_dnodes=4
 rep_factor = 2
 json_of_files={}
-
 datanode={}
 
 def hashing(file_block_no, num_of_datanodes):
 	# return (file_block_no % num_of_datanodes)+1
-	out=file_block_no % num_of_datanodes
-	if(datanode[out+1]>0):
-		return out+1
+	hash_val = (file_block_no % num_of_datanodes)
+	if(datanode[hash_val+1]>0):
+		return hash_val+1
 	else:
-		j=out+1
+		j=hash_val+1
 		while(datanode[j]==0 and j<num_of_datanodes):
 			j=j+1
 		if(datanode[j]!=0):
 			return j
 		else:
 			return -1
-# creates direcories which act as datanodes 
-def datanode_creation(num_of_datanode,datanode_size):
-	for data_node_number in range(1,num_of_dnodes+1):
-		os.mkdir('Datanodes/'+str(data_node_number)+"_data_node")
-		datanode[data_node_number]=datanode_size
-datanode_creation(num_of_dnodes,4)  
+
+
+# creates directories which act as datanodes 
+def create_datanodes(num_datanodes,datanode_size):
+	for dnode in range(1,num_datanodes+1):
+		os.mkdir('Datanodes/'+str(dnode)+"_data_node")
+		datanode[dnode]=datanode_size
+
+
+create_datanodes(num_of_dnodes,4)  
 
 # creates a chunk of file
-def split_files(filename, chunksize,datanode_size,rep_factor):
-	# try:
-	# 	logs = open('Namenodes/sample.json')
-	# except Exception as e:
-	# 	logs = open('Namenodes/sample.json')
-	# json_of_files = json.load(logs)
-	# logs=open('Namenodes/sample.json','w')
+def split_files(filename,block_size,datanode_size,replication_factor):
 	with open(filename, 'rb') as bytefile:
 		json_of_files[filename]={}
 		content = bytearray(os.path.getsize(filename))
 		bytefile.readinto(content)
-		num_of_blocks_in_input=len(content)/chunksize
+		file_splits=len(content)/block_size
 		available_blocks=0
 		for key,value in datanode.items():
 			available_blocks+=value
-		if(available_blocks<num_of_blocks_in_input):
-			print('Less blocks available')
+		if(available_blocks<file_splits):
+			print('Not enough space to store the file!')
 			return
-		for count, i in enumerate(range(0, len(content), chunksize)):
-			json_of_files[filename][count+1]={}
-			hashed = hashing(count,num_of_dnodes)
-			if(hashed==-1):
-				print('No space')
+		for block_number, i in enumerate(range(0, len(content), block_size)):
+			json_of_files[filename][block_number+1]={}
+			hashed_val = hashing(block_number,num_of_dnodes)
+			if(hashed_val==-1):
+				print('Not space to store the file!')
 				return
-			block_no_in_dn = datanode_size-datanode[hashed]+1
-			with open(f'Datanodes/{hashed}' + '_data_node/' + str(block_no_in_dn) + '.txt', 'wb') as fh:
-				json_of_files[str(filename)][count+1][hashed]=block_no_in_dn
-				datanode[hashed]-=1
-				fh.write(content[i: i + chunksize])
+			dnode_block = datanode_size-datanode[hashed_val]+1
+			with open(f'Datanodes/{hashed_val}' + '_data_node/' + str(dnode_block) + '.txt', 'wb') as fh:
+				json_of_files[str(filename)][block_number+1][hashed_val]=block_number
+				datanode[hashed_val]-=1
+				fh.write(content[i: i + block_size])
 			fh.close()
 		bytefile.close()
 
 		remaining_blocks=0
 		for key,value in datanode.items():
-			remaining_blocks+=value
-		replica_fac=min(rep_factor-1,int(remaining_blocks/num_of_blocks_in_input))
-		if(replica_fac<1):
-			print('No space to replicate')
+			remaining_blocks += value
+		can_replicate=min(replication_factor-1,int(remaining_blocks/file_splits))
+		if(can_replicate<1):
+			print('No space to replicate!')
 			return
-		for count, i in enumerate(range(0, len(content), chunksize)):
-			hashed = hashing(count,num_of_dnodes)
-			input_block=json_of_files[str(filename)][count+1][hashed]
-			with open(f'Datanodes/{hashed}' + '_data_node/' + str(input_block) + '.txt', 'rb') as bytefile:
+		for block_number, i in enumerate(range(0, len(content), block_size)):
+			hashed_val = hashing(block_number,num_of_dnodes)
+			curr_block = json_of_files[str(filename)][block_number+1][hashed_val]
+			with open(f'Datanodes/{hashed_val}' + '_data_node/' + str(curr_block) + '.txt', 'rb') as bytefile:
 				content = bytearray(os.path.getsize(filename))
 				bytefile.readinto(content)
-				for replica_count in range(1,replica_fac+1):
-					hashed_replica=((hashed-1+replica_count)%datanode_size)+1
-					block_no_in_dn_replica = datanode_size-datanode[hashed_replica]+1
-					with open(f'Datanodes/{hashed_replica}' + '_data_node/' + str(block_no_in_dn_replica) + '.txt', 'wb') as fh:
-						json_of_files[str(filename)][count+1][hashed_replica]=block_no_in_dn_replica
-						datanode[hashed_replica]-=1
-						fh.write(content[i: i + chunksize])
+				for count_replicate in range(1,can_replicate+1):
+					next_dnode=((hashed_val-1+count_replicate)%datanode_size)+1
+					next_dnode_block = datanode_size-datanode[next_dnode]+1
+					with open(f'Datanodes/{next_dnode}' + '_data_node/' + str(next_dnode_block) + '.txt', 'wb') as fh:
+						json_of_files[str(filename)][block_number+1][next_dnode]=next_dnode_block
+						datanode[next_dnode]-=1
+						fh.write(content[i: i + block_size])
 					fh.close()
 			bytefile.close()
 
